d6150142d8841552ebe84617231baf75dccccf87
oyesola-dolapo <oyesolabolamide@gmail.com>
2024-03-01 23:27:49 +0100

    commit

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 export default function Landing() {
   return (
-    <div className="w-full h-screen relative flex px-[.6rem] bg-black">
+    <div className="w-full h-[90vh] lg:h-screen relative flex px-[.6rem] bg-black">
       <img
         src="/images/bg-img.webp"
         alt=""
         className="absolute top-0 left-0 w-full h-full object-cover opacity-[.6] z-[0]"
       />
-      <div className="z-[0] m-auto text-center text-white">
+      <div className="z-[0] m-auto text-center text-white lg:pt-[4rem]">
         <h1 className="text-white uppercase text-[1.8rem] lg:text-[4rem] font-bold">
           welcome to inri evangelical spiritual{" "}
           <span className="text-webColor">church</span>
@@ -23,7 +23,7 @@ export default function Landing() {
           </p>
           <p>- 1 Corinthians 14:26</p>
         </div>
-        <button className="border-2 border-solid border-webColor px-8 py-3 mt-6 ">
+        <button className="border-2 border-solid border-webColor px-8 py-3 mt-8 bg-[rgba(255, 255, 255, 0.5)] backdrop-filter backdrop-blur-md">
           worship with us
         </button>
       </div>
