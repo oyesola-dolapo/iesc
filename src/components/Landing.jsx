8a9c654332801c4cb4ce8127a838b66586085270
oyesola-dolapo <oyesolabolamide@gmail.com>
2024-03-06 03:13:56 +0100

    commit

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,6 +1,18 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 export default function Landing() {
+  const myVariants = {
+    hidden: {
+      opacity: 0,
+      y: 120,
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+    },
+  };
+
   return (
     <div className="w-full h-[90vh] sm:h-screen relative flex px-[.6rem] sm:px-[2.5rem] bg-black">
       <img
@@ -9,11 +21,21 @@ export default function Landing() {
         className="absolute top-0 left-0 w-full h-full object-cover opacity-[.6] z-[0]"
       />
       <div className="z-[0] m-auto text-center text-white lg:pt-[4rem]">
-        <h1 className="text-white uppercase text-[1.8rem] sm:text-[3rem] lg:text-[4rem] font-bold">
+        <motion.h1
+          variants={myVariants}
+          initial="hidden"
+          animate="visible"
+          transition={{ duration: 1 }}
+          className="text-white uppercase text-[1.8rem] sm:text-[3rem] lg:text-[4rem] font-bold">
           welcome to inri evangelical spiritual{" "}
           <span className="text-webColor">church</span>
-        </h1>
-        <div className="font-medium mt-4 sm:text-[1.5rem] lg:px-[10rem]">
+        </motion.h1>
+        <motion.div
+          variants={myVariants}
+          initial="hidden"
+          animate="visible"
+          transition={{ duration: 1, delay: 0.5 }}
+          className="font-medium mt-4 sm:text-[1.5rem] lg:px-[10rem]">
           <p>
             {" "}
             "What then shall we say, brothers and sisters? When you come
@@ -22,10 +44,15 @@ export default function Landing() {
             so that the church may be built up."
           </p>
           <p>- 1 Corinthians 14:26</p>
-        </div>
-        <button className="worship-btn isolate relative uppercase border-2 border-solid border-webColor px-8 py-3 mt-8 backdrop-filter backdrop-blur-sm ">
+        </motion.div>
+        <motion.button
+          variants={myVariants}
+          initial="hidden"
+          animate="visible"
+          transition={{ duration: 1, delay: 1 }}
+          className="worship-btn isolate relative uppercase border-2 border-solid border-webColor px-8 py-3 mt-8 backdrop-filter backdrop-blur-sm ">
           worship with us
-        </button>
+        </motion.button>
       </div>
     </div>
   );
