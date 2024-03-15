f6077fcde4461296a128066662468e871a92222c
oyesola-dolapo <oyesolabolamide@gmail.com>
2024-03-15 19:06:20 +0100

    commit

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -14,7 +14,7 @@ export default function Landing() {
   };
 
   return (
-    <div className="w-full min-h-[90vh] py-[4rem] h-max sm:h-max relative flex px-[.6rem] lg:h-screen sm:px-[2.5rem] bg-black">
+    <div className="w-full min-h-[90vh] py-[4rem] h-max sm:h-max relative flex px-[.6rem] xl:h-screen sm:px-[2.5rem] bg-black">
       <img
         src="/images/bg-img.webp"
         alt=""
