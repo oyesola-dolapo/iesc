3aaa55bb32559982a7ad9cac68025482763c5870
oyesola-dolapo <oyesolabolamide@gmail.com>
2024-03-26 13:45:43 +0100

    commit

--- src/components/History/History.jsx
+++ src/components/History/History.jsx
@@ -2,44 +2,53 @@ import React from "react";
 
 export default function History() {
   return (
-    <div className="pt-[4.5rem] lg:w-[60%] px-[1rem] lg:px-[2rem]">
-      <h1 className="tracking-wider font-bold text-[1.5rem] mb-[.5rem] xl:text-[2rem]">
-        CHURCH HISTORY
-      </h1>
-      <p className="lg:text-[1.2rem]">
-        The Church of God known as{" "}
-        <span className="text-webColor font-medium">
-          INRI EVANGELICAL SPIRITUAL CHURCH
-        </span>{" "}
-        started in a one room apartment at No 27 Gbadamosi Commercial Street,
-        Ilasamaja area of Lagos on the 10th <br /> <br />
-        The name of the Church was revealed by the Holy Spirit and the
-        leadership constituted through Divine guidance before that day. There
-        was the mighty presence of the Heavenly Hosts during the exercises; this
-        was in the month of January, 1994. <br /> <br />
-        There was no adequate space for us to conduct the service here thus on
-        the 31st December 1994, we moved to house Number 1, Mabinuori Street in
-        the same Ilasamaja. <br /> <br />
-        We tested the nature and the spiritual strength of the Church on the
-        last Sunday of the month of November, 1994. This took place at the Ilasa
-        Court, Baale Street. Important personalities were invited to the service{" "}
-        <br /> <br />
-        <span className="text-webColor font-medium">
-          INRI EVANGELICAL SPIRITUAL CHURCH
-        </span>{" "}
-        is not a member of the number of the Christ Apostolic Church, the
-        Cherubim and Seraphim Church, the Celestial Church of Christ or the
-        member of any white garment church of any description. The Church is
-        standing on it's own with it's own unique identity. <br /> <br />
-        At No 1, Mabinuori Street where we moved to on the 31st December, 1994,
-        we could not put up any permanent structure, We operated under a shed
-        covered with palm fronds. Later, we levied ourselves to be able to put a
-        small plank structure at the venue. The floor was filled with Beach
-        sand. That was the situation for the 5years we spend on this site.{" "}
-        <br /> <br /> We moveed to Oke Afa and specifically to our own parcel of
-        land at Primate Ayodele Crescent, Inri Bus-Stop Oke Afe where we also
-        has our first service on this ground in December 200
-      </p>
+    <div>
+      <div className="relative bg-black w-[100vw] mb-[1rem]">
+        <img
+          src="/images/building/IMG_2583.webp"
+          alt=""
+          className="h-[35vh] xl:h-[50vh] w-[100vw] object-cover opacity-[.5]  mb-[1rem]"
+        />
+        <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 tracking-wider w-max mt-[1rem] text-white font-bold text-[1.5rem] xl:text-[2.5rem] p-0">
+          church history
+        </h1>
+      </div>
+      <div className=" lg:w-[60%] px-[1rem] lg:px-[2rem]">
+        <p className="lg:text-[1.2rem]">
+          The Church of God known as{" "}
+          <span className="text-webColor font-medium">
+            INRI EVANGELICAL SPIRITUAL CHURCH
+          </span>{" "}
+          started in a one room apartment at No 27 Gbadamosi Commercial Street,
+          Ilasamaja area of Lagos on the 10th <br /> <br />
+          The name of the Church was revealed by the Holy Spirit and the
+          leadership constituted through Divine guidance before that day. There
+          was the mighty presence of the Heavenly Hosts during the exercises;
+          this was in the month of January, 1994. <br /> <br />
+          There was no adequate space for us to conduct the service here thus on
+          the 31st December 1994, we moved to house Number 1, Mabinuori Street
+          in the same Ilasamaja. <br /> <br />
+          We tested the nature and the spiritual strength of the Church on the
+          last Sunday of the month of November, 1994. This took place at the
+          Ilasa Court, Baale Street. Important personalities were invited to the
+          service <br /> <br />
+          <span className="text-webColor font-medium">
+            INRI EVANGELICAL SPIRITUAL CHURCH
+          </span>{" "}
+          is not a member of the number of the Christ Apostolic Church, the
+          Cherubim and Seraphim Church, the Celestial Church of Christ or the
+          member of any white garment church of any description. The Church is
+          standing on it's own with it's own unique identity. <br /> <br />
+          At No 1, Mabinuori Street where we moved to on the 31st December,
+          1994, we could not put up any permanent structure, We operated under a
+          shed covered with palm fronds. Later, we levied ourselves to be able
+          to put a small plank structure at the venue. The floor was filled with
+          Beach sand. That was the situation for the 5years we spend on this
+          site. <br /> <br /> We moveed to Oke Afa and specifically to our own
+          parcel of land at Primate Ayodele Crescent, Inri Bus-Stop Oke Afe
+          where we also has our first service on this ground in December 200
+        </p>
+      </div>
     </div>
   );
 }
