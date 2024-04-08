26639b917008fe084acf3f1e2a293e4019eb79f1
oyesola-dolapo <oyesolabolamide@gmail.com>
2024-04-08 22:30:40 +0100

    mobile version for parishes

--- src/components/Programs/Sunday/Sunday.jsx
+++ src/components/Programs/Sunday/Sunday.jsx
@@ -20,17 +20,8 @@ export default function Sunday() {
           time: "Last Service: 11:30am - 3:30pm",
         },
       ],
-      location: (
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.954501452327!2d3.3114911747533022!3d6.527430893465217!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8e5f29ff59bf%3A0xe1922c4a94d72886!2s2%20Primate%20Ayodele%20Cres%2C%20Isaga%20Tedo%2C%20Close%20102214%2C%20Lagos!5e0!3m2!1sen!2sng!4v1709333354852!5m2!1sen!2sng"
-          width="600"
-          height="450"
-          style={{ border: "0" }}
-          allowfullscreen=""
-          loading="lazy"
-          referrerpolicy="no-referrer-when-downgrade"
-          className="w-full"></iframe>
-      ),
+      location:
+        "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.954501452327!2d3.3114911747533022!3d6.527430893465217!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8e5f29ff59bf%3A0xe1922c4a94d72886!2s2%20Primate%20Ayodele%20Cres%2C%20Isaga%20Tedo%2C%20Close%20102214%2C%20Lagos!5e0!3m2!1sen!2sng!4v1709333354852!5m2!1sen!2sng",
     },
 
     {
@@ -41,17 +32,8 @@ export default function Sunday() {
           time: "First Service: 10:00am - 12:00pm",
         },
       ],
-      location: (
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.954501452327!2d3.3114911747533022!3d6.527430893465217!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8e5f29ff59bf%3A0xe1922c4a94d72886!2s2%20Primate%20Ayodele%20Cres%2C%20Isaga%20Tedo%2C%20Close%20102214%2C%20Lagos!5e0!3m2!1sen!2sng!4v1709333354852!5m2!1sen!2sng"
-          width="600"
-          height="450"
-          style={{ border: "0" }}
-          allowfullscreen=""
-          loading="lazy"
-          referrerpolicy="no-referrer-when-downgrade"
-          className="w-full"></iframe>
-      ),
+      location:
+        "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.954501452327!2d3.3114911747533022!3d6.527430893465217!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8e5f29ff59bf%3A0xe1922c4a94d72886!2s2%20Primate%20Ayodele%20Cres%2C%20Isaga%20Tedo%2C%20Close%20102214%2C%20Lagos!5e0!3m2!1sen!2sng!4v1709333354852!5m2!1sen!2sng",
     },
     {
       title: "Sope parish",
@@ -61,27 +43,29 @@ export default function Sunday() {
           time: "First Service: 10:00am - 12:00pm",
         },
       ],
-      location: (
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.954501452327!2d3.3114911747533022!3d6.527430893465217!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8e5f29ff59bf%3A0xe1922c4a94d72886!2s2%20Primate%20Ayodele%20Cres%2C%20Isaga%20Tedo%2C%20Close%20102214%2C%20Lagos!5e0!3m2!1sen!2sng!4v1709333354852!5m2!1sen!2sng"
-          width="600"
-          height="430"
-          style={{ border: "0" }}
-          allowfullscreen=""
-          loading="lazy"
-          referrerpolicy="no-referrer-when-downgrade"
-          className="w-full"></iframe>
-      ),
+      location:
+        "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.954501452327!2d3.3114911747533022!3d6.527430893465217!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8e5f29ff59bf%3A0xe1922c4a94d72886!2s2%20Primate%20Ayodele%20Cres%2C%20Isaga%20Tedo%2C%20Close%20102214%2C%20Lagos!5e0!3m2!1sen!2sng!4v1709333354852!5m2!1sen!2sng",
+    },
+    {
+      title: "Sope parish",
+      subhead: "service time",
+      times: [
+        {
+          time: "First Service: 10:00am - 12:00pm",
+        },
+      ],
+      location:
+        "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.954501452327!2d3.3114911747533022!3d6.527430893465217!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8e5f29ff59bf%3A0xe1922c4a94d72886!2s2%20Primate%20Ayodele%20Cres%2C%20Isaga%20Tedo%2C%20Close%20102214%2C%20Lagos!5e0!3m2!1sen!2sng!4v1709333354852!5m2!1sen!2sng",
     },
   ];
 
   return (
-    <div className="px-[1rem] xl:px-[2rem] pt-[4.5rem] flex min-h-[90vh]">
-      <div className="static w-screen hidden lg:flex ">
-        <div className="w-[30%] overflow-y-scroll">
+    <div className="pt-[4.5rem] flex">
+      <div className="static w-screen sm:flex h-max ">
+        <div className="service-scroll h-[80vh] sm:h-[60vh] lg:h-[80vh] sm:w-[40%] lg:w-[30%] overflow-hidden overflow-y-scroll">
           {services.map((service) => {
             return (
-              <div className="mb-[2rem]">
+              <div className="mb-[.5rem] mx-[.5rem] p-[1rem] bg-gray-900 text-white">
                 <h1 className="uppercase tracking-wider text-textGold font-bold text-[1.5rem]">
                   {service.title}
                 </h1>{" "}
@@ -101,7 +85,7 @@ export default function Sunday() {
                   onClick={() => {
                     setLocation(service.location);
                   }}
-                  className="bg-webColor font-bold tracking-wider mt-[.6rem] px-[1.6rem] py-[.3rem] rounded">
+                  className="bg-webColor text-gray-900 font-bold tracking-wider mt-[.6rem] px-[1.6rem] py-[.3rem] rounded">
                   Locate
                 </button>
               </div>
@@ -109,7 +93,17 @@ export default function Sunday() {
           })}
         </div>
 
-        <div className="w-[70%]">{location}</div>
+        <div className="sm:w-[60%] lg:w-[70%] sm:h-full px-[.5rem]">
+          <iframe
+            src={location}
+            width="600"
+            height="450"
+            style={{ border: "0" }}
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+            className="w-full h-[25rem] sm:h-full"></iframe>
+        </div>
       </div>
     </div>
   );
