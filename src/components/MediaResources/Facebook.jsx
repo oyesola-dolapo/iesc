2175aa53492397bd0fb177a14ce472e6e562c8a3
oyesola-dolapo <oyesolabolamide@gmail.com>
2024-06-25 14:19:04 +0100

    added search function to media

--- src/components/MediaResources/Facebook.jsx
+++ src/components/MediaResources/Facebook.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { db } from "../../config/firebase";
-import { collection, getDocs, doc } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
 
-export default function Facebook() {
+export default function Facebook({ active }) {
   const [links, setLinks] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
 
   const linkCollectionRef = collection(db, "facebook");
 
@@ -22,38 +23,52 @@ export default function Facebook() {
     };
 
     getLink();
-  }, []);
+  }, [linkCollectionRef]);
+
+  const handleSearchChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const filteredLinks = links.filter((link) =>
+    link.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
   return (
     <section
       id="Facebook"
-      className="mt-[1rem] lg:mt-[1.5rem] px-[1rem] lg:px-[2rem]">
-      {/* <h2
-        className={`${
-          links.length === 0 && "hidden"
-        } text-center mb-[.5rem] text-[1rem] lg:text-[1.2rem] font-bold`}>
-        FACEBOOK VIDEOS
-      </h2> */}
+      className={`mt-[1rem] lg:mt-[1.5rem] px-[1rem] lg:px-[2rem] ${
+        active ? "" : "hidden"
+      }`}>
+      <div className="flex justify-center mb-4">
+        <input
+          type="text"
+          placeholder="Search video by title..."
+          value={searchQuery}
+          onChange={handleSearchChange}
+          className=" border-2 border-gray-300 rounded-md px-3 py-1 w-full lg:w-[50%] h-[3rem] focus:outline-webColor focus:outline-1"
+        />
+      </div>
       <div
         className={`${
-          links.length > 2 ? "lg:justify-between" : ""
-        } sm:flex sm:justify-between lg:gap-[1.5rem] lg:flex-wrap lg:w-[90%] lg:mx-auto`}>
-        {links.map((link) => {
-          return (
-            <div
-              className="sm:w-[49%] lg:w-[30%] mb-[1rem] lg:mb-0"
-              key={link.id}>
-              <iframe
-                src={link.link}
-                scrolling="no"
-                frameborder="0"
-                allowFullScreen
-                allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
-                className="w-full h-[12.5rem] lg:h-[14rem]"></iframe>{" "}
-              <p className="text-[.9rem] lg:text-[1rem]">{link.title}</p>
-            </div>
-          );
-        })}
+          filteredLinks.length > 2 ? "lg:justify-between" : ""
+        } sm:flex sm:justify-start lg:gap-[1.5rem] lg:flex-wrap lg:w-[90%] lg:mx-auto`}>
+        {filteredLinks.map((link) => (
+          <div
+            className="sm:w-[49%] lg:w-[30%] mb-[1rem] lg:mb-0"
+            key={link.id}>
+            <iframe
+              src={link.link}
+              scrolling="no"
+              frameBorder="0"
+              allowFullScreen
+              allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+              className="w-full h-[12.5rem] lg:h-[14rem]"></iframe>{" "}
+            <p className="text-[.9rem] lg:text-[1rem]">{link.title}</p>
+          </div>
+        ))}
+        {filteredLinks.length === 0 && (
+          <p className="text-center w-full mt-4">No videos found.</p>
+        )}
       </div>
     </section>
   );
--- src/components/MediaResources/Youtube.jsx
+++ src/components/MediaResources/Youtube.jsx
@@ -1,16 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { db } from "../../config/firebase";
-import { collection, getDocs, doc } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
 import { toast } from "react-toastify";
 
-export default function Youtube() {
+export default function Youtube({ active }) {
   const [links, setLinks] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
 
   const linkCollectionRef = collection(db, "youtube");
 
   useEffect(() => {
-    toast.info("Loading...", {autoClose: 600});
-
     const getLink = async () => {
       try {
         const data = await getDocs(linkCollectionRef);
@@ -25,43 +24,51 @@ export default function Youtube() {
     };
 
     getLink();
-  }, []);
+  }, [linkCollectionRef]);
+
+  const handleSearchChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const filteredLinks = links.filter((link) =>
+    link.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
   return (
-    <section id="Youtube" className=" px-[1rem] lg:px-[2rem]">
-      {/* <h2
-        className={`${
-          links.length === 0 && "hidden"
-        } text-center mb-[.5rem] text-[1rem] lg:text-[1.2rem] font-bold`}>
-        YOUTUBE VIDEOS
-      </h2> */}
+    <section
+      id="Youtube"
+      className={`px-[1rem] lg:px-[2rem] ${active ? "" : "hidden"}`}>
+      <div className="flex justify-center mb-4">
+        <input
+          type="text"
+          placeholder="Search video by title..."
+          value={searchQuery}
+          onChange={handleSearchChange}
+          className=" border-2 border-gray-300 rounded-md px-3 py-1 w-full lg:w-[50%] h-[3rem] focus:outline-webColor focus:outline-1"
+        />
+      </div>
       <div
         className={`${
-          links.length > 2 ? "lg:justify-between" : ""
-        } sm:flex sm:justify-between lg:gap-[1.5rem] sm:flex-wrap lg:w-[90%] lg:mx-auto`}>
-        {links.map((link) => {
-          return (
-            // <div
-            //   key={link.link}
-            //   className="youtube-container"
-            //   dangerouslySetInnerHTML={{ __html: link.link }}
-            // />
-
-            <div
-              className="sm:w-[49%] lg:w-[30%] mb-[1rem] lg:mb-0"
-              key={link.id}>
-              <iframe
-                src={link.link}
-                title="YouTube video player"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                referrerpolicy="strict-origin-when-cross-origin"
-                allowFullScreen="true"
-                className="w-full h-[15rem] lg:h-[15rem]"></iframe>{" "}
-              <p className="text-[.9rem] lg:text-[1rem]">{link.title}</p>
-            </div>
-          );
-        })}
+          filteredLinks.length > 2 && "lg:justify-between"
+        } sm:flex sm:justify-start lg:gap-[1.5rem] sm:flex-wrap lg:w-[90%] lg:mx-auto`}>
+        {filteredLinks.map((link) => (
+          <div
+            className="sm:w-[49%] lg:w-[30%] mb-[1rem] lg:mb-0"
+            key={link.id}>
+            <iframe
+              src={link.link}
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerpolicy="strict-origin-when-cross-origin"
+              allowFullScreen="true"
+              className="w-full h-[15rem] lg:h-[15rem]"></iframe>{" "}
+            <p className="text-[.9rem] lg:text-[1rem]">{link.title}</p>
+          </div>
+        ))}
+        {filteredLinks.length === 0 && (
+          <p className="text-center w-full mt-4">No videos found.</p>
+        )}
       </div>
     </section>
   );
